8dd8522fdd17dc9ae2960e1912d3b3feed8ebded
thnguyen290106 <thnguyen290106@gmail.com>
2025-10-31 13:52:04 +0700

    Optimized Bai01

--- Bai01/Bai01Dashboard.Designer.cs
+++ Bai01/Bai01Dashboard.Designer.cs
@@ -11,65 +11,49 @@
 
         private void InitializeComponent()
         {
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.btnUDPServer = new System.Windows.Forms.Button();
-            this.btnUDPClient = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold);
-            this.lblTitle.ForeColor = System.Drawing.Color.Navy;
-            this.lblTitle.Location = new System.Drawing.Point(100, 20);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(200, 32);
-            this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "Bài 01 - UDP";
-            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            btnUDPServer = new Button();
+            btnUDPClient = new Button();
+            SuspendLayout();
             // 
             // btnUDPServer
             // 
-            this.btnUDPServer.BackColor = System.Drawing.Color.LightBlue;
-            this.btnUDPServer.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnUDPServer.ForeColor = System.Drawing.Color.White;
-            this.btnUDPServer.Location = new System.Drawing.Point(40, 80);
-            this.btnUDPServer.Name = "btnUDPServer";
-            this.btnUDPServer.Size = new System.Drawing.Size(130, 50);
-            this.btnUDPServer.TabIndex = 1;
-            this.btnUDPServer.Text = "UDP Server";
-            this.btnUDPServer.UseVisualStyleBackColor = false;
-            this.btnUDPServer.Click += new System.EventHandler(this.btnUDPServer_Click);
+            btnUDPServer.BackColor = Color.LightBlue;
+            btnUDPServer.Font = new Font("Arial", 12F, FontStyle.Bold);
+            btnUDPServer.ForeColor = Color.White;
+            btnUDPServer.Location = new Point(42, 58);
+            btnUDPServer.Name = "btnUDPServer";
+            btnUDPServer.Size = new Size(130, 50);
+            btnUDPServer.TabIndex = 1;
+            btnUDPServer.Text = "UDP Server";
+            btnUDPServer.UseVisualStyleBackColor = false;
+            btnUDPServer.Click += btnUDPServer_Click;
             // 
             // btnUDPClient
             // 
-            this.btnUDPClient.BackColor = System.Drawing.Color.LightBlue;
-            this.btnUDPClient.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnUDPClient.ForeColor = System.Drawing.Color.White;
-            this.btnUDPClient.Location = new System.Drawing.Point(210, 80);
-            this.btnUDPClient.Name = "btnUDPClient";
-            this.btnUDPClient.Size = new System.Drawing.Size(130, 50);
-            this.btnUDPClient.TabIndex = 2;
-            this.btnUDPClient.Text = "UDP Client";
-            this.btnUDPClient.UseVisualStyleBackColor = false;
-            this.btnUDPClient.Click += new System.EventHandler(this.btnUDPClient_Click);
+            btnUDPClient.BackColor = Color.LightBlue;
+            btnUDPClient.Font = new Font("Arial", 12F, FontStyle.Bold);
+            btnUDPClient.ForeColor = Color.White;
+            btnUDPClient.Location = new Point(212, 58);
+            btnUDPClient.Name = "btnUDPClient";
+            btnUDPClient.Size = new Size(130, 50);
+            btnUDPClient.TabIndex = 2;
+            btnUDPClient.Text = "UDP Client";
+            btnUDPClient.UseVisualStyleBackColor = false;
+            btnUDPClient.Click += btnUDPClient_Click;
             // 
             // Bai01DashboardForm
             // 
-            this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(384, 181);
-            this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.btnUDPServer);
-            this.Controls.Add(this.btnUDPClient);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.Name = "Bai01DashboardForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Bài 01 - UDP";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            BackColor = Color.WhiteSmoke;
+            ClientSize = new Size(384, 181);
+            Controls.Add(btnUDPServer);
+            Controls.Add(btnUDPClient);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Bai01DashboardForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Bài 01 - UDP";
+            ResumeLayout(false);
         }
-        private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Button btnUDPServer;
         private System.Windows.Forms.Button btnUDPClient;
     }
